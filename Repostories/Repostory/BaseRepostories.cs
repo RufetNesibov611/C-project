6f1af5bb5429cc88c94597e1b7ba8c073a6617f5
RufetNesibov611 <rufaten@code.edu.az>
2023-11-22 01:25:06 +0400

    added project first

---
+++ Repostories/Repostory/BaseRepostories.cs
@@ -0,0 +1,37 @@
+﻿using Repostories.Repostory.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repostories.Repostory
+{
+    public class BaseRepostories<T> : IBaseRepostories<T> where T : 
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delet(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Edit(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repostories/Repostory/Interface/IBaseRepostories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repostories.Repostory.Interface
+{
+    public interface IBaseRepostories<T>
+    {
+        void Create(T entity);
+        void Delet(T entity);
+        List<T> Edit(T entity);
+        List<T> GetById(int id);
+        List<T> GetAll ();
+
+
+
+    }
+}
